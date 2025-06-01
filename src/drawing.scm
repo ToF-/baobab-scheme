615fd741f29fa7bbd61609096aca3043d98e6af8
Christophe Thibaut <cthibauttof@gmail.com>
2025-06-01 23:44:35 +0200

    baobab with square angle crotch

--- src/drawing.scm
+++ src/drawing.scm
@@ -40,29 +40,42 @@
         (dy (- y1 y0)))
     (sqrt (+ (square dx) (square dy)))))
 
+(define (choose a b)
+  (+ a (random (- b a) seed)))
+
 (define (draw-squares x y angle unit)
   (let* ((vert (+ angle (/ PI 2.0)))
          (x-upper-left (+ x (* (cos vert) unit)))
          (y-upper-left (- y (* (sin vert) unit)))
          (x-upper-right (+ x-upper-left (* (cos angle) unit)))
          (y-upper-right (- y-upper-left (* (sin angle) unit)))
-         (alpha (+ (/ PI 12.0) (random PI seed)))
+         (alpha (choose (/ PI 6.0) (/ PI 1.2)))
+         (angle-prime (+ angle (/ alpha 2.0)))
+         (angle-second (- angle-prime (/ PI 2.0)))
          (half-unit (/ unit 2.0))
          (x-middle (+ x-upper-left (* (cos angle) half-unit)))
          (y-middle (- y-upper-left (* (sin angle) half-unit)))
-         (x-top (+ x-middle (* (cos (- angle alpha)) half-unit)))
-         (y-top (+ y-middle (* (sin (- angle alpha)) half-unit))))
-    (begin
-      (draw-square x y angle unit)
-      (draw-square x-upper-left y-upper-left (- alpha (/ PI 2.0)) (distance x-upper-left y-upper-left x-top y-top))
-      (draw-square x-top y-top (+ alpha (/ PI 2.0)) (distance x-top y-top x-upper-right y-upper-right))
-      )))
+         (x-top (+ x-middle (* (cos (+ angle alpha)) half-unit)))
+         (y-top (- y-middle (* (sin (+ angle alpha)) half-unit)))
+         (unit-prime (distance x-upper-left y-upper-left x-top y-top))
+         (unit-second (distance x-top y-top x-upper-right y-upper-right)))
+    ; h² = a² + o² → o² = h² - a² → o = √(h²-a²)
+
+    (cond
+      ((> unit 4.0)
+       (begin
+         (draw-square x y angle unit)
+         (move-to x-upper-left y-upper-left)
+         (draw-squares x-upper-left y-upper-left angle-prime unit-prime)
+         (draw-squares x-top y-top angle-second unit-second) 
+         ))
+      (else ()))))
 
 (define (print-html-canvas)
   (begin
   (print-html-canvas-header 1000 600)
   (format #t "  ctx.beginPath();~%")
-  (draw-squares 500.0 600.0 (/ PI 48.0) 100.0)
+  (draw-squares 500.0 600.0 (choose 0.0 (/ PI 24.0)) 100.0)
   (format #t "  ctx.stroke();~%")
   (print-html-canvas-footer)))
 
